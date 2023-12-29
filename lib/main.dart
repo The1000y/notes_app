10c5c7b9eb3eec7721f2725fa54c0d0fc1d345df
Mohamed Elalfy <mohamedelalfy378@gmail.com>
2023-12-29 17:38:21 +0200

    create new

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Note_app());
+  runApp(const Nooe_app());
 }
 
-class Note_app extends StatelessWidget {
-  const Note_app({super.key});
+class Nooe_app extends StatelessWidget {
+  const Nooe_app({super.key});
 
   @override
   Widget build(BuildContext context) {
