7e1a0165358bac3866046252b41916c2af047807
Mohamed Elalfy <mohamedelalfy378@gmail.com>
2024-01-05 15:55:34 +0200

    Create Read Note Cubit

--- lib/read_notes_cubit/cubit/read_notes_cubit.dart
+++ lib/read_notes_cubit/cubit/read_notes_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constains.dart';
 import 'package:notes_app/model/note_model.dart';
 
 part 'read_notes_state.dart';
 
 class ReadNotesCubit extends Cubit<ReadNotesState> {
   ReadNotesCubit() : super(ReadNotesInitial());
+
+  featchAllNotes() {
+    try {
+      var box = Hive.box<NoteModel>(kPrimaryBoxName);
+      List<NoteModel> boxValue = box.values.toList();
+      emit(ReadNotesSuccess(boxValue));
+    } catch (e) {
+      emit(ReadNotesFauiler(e.toString()));
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/read_notes_cubit/cubit/read_notes_cubit.dart';
 import 'package:notes_app/widgets/add_buttom_model_sheet.dart';
 import 'package:notes_app/widgets/notes_view_body_widget.dart';
 
@@ -8,21 +10,24 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showModalBottomSheet(
-                isScrollControlled: true,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                context: context,
-                builder: (conrext) {
-                  return const AddNoteButtomSheet();
-                });
-          },
-          child: const Icon(Icons.add)),
-      body: const NotesViewBody(),
+    return BlocProvider(
+      create: (context) => ReadNotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  context: context,
+                  builder: (conrext) {
+                    return const AddNoteButtomSheet();
+                  });
+            },
+            child: const Icon(Icons.add)),
+        body: const NotesViewBody(),
+      ),
     );
   }
 }
