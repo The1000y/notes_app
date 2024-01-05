fd26b70980fd5ad7d1096a5cd77e929b82643ad0
Mohamed Elalfy <mohamedelalfy378@gmail.com>
2024-01-05 14:40:29 +0200

    Make Modal Buttom Sheet  isScrollControlled: true,

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -12,8 +12,10 @@ class NotesView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             showModalBottomSheet(
+                isScrollControlled: true,
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16)),
+                  borderRadius: BorderRadius.circular(16),
+                ),
                 context: context,
                 builder: (conrext) {
                   return const AddNoteButtomSheet();
