8c121bbb84027f3fa800dc6b3a286882cd11017f
Mohamed Elalfy <mohamedelalfy378@gmail.com>
2024-01-05 15:43:49 +0200

    Create State and Cubit of Read Notes Cubit

---
+++ lib/read_notes_cubit/cubit/read_notes_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:notes_app/model/note_model.dart';
+
+part 'read_notes_state.dart';
+
+class ReadNotesCubit extends Cubit<ReadNotesState> {
+  ReadNotesCubit() : super(ReadNotesInitial());
+}
